f96861982c05078081b4ac7886bfa7f704b3a63e
kartikey151999 <kartikeypande123@gmail.com>
2026-09-06 12:38:08 +0530

    include template in another template

--- staticFile_tempInheritance/course/urls.py
+++ staticFile_tempInheritance/course/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import about_django,about_fastapi,Home
 urlpatterns = [
-  path('',Home),
-  path('django/',about_django),
-  path('fastapi/',about_fastapi)
+  path('',Home,name='home'),
+  path('django/',about_django,name='course'),
+  path('fastapi/',about_fastapi,name='course')
 ]
--- staticFile_tempInheritance/course_fees/urls.py
+++ staticFile_tempInheritance/course_fees/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import django_fees,fastapi_fees
 urlpatterns=[
-  path('django/',django_fees),
-  path('fastapi/',fastapi_fees)
+  path('django/',django_fees,name='fees'),
+  path('fastapi/',fastapi_fees ,name= 'fees')
 
 ]
